a7a101c116692e548e2c0a1eaf358f3b0a27df6b
Ashwith <ashwithreddy01@gmail.com>
2026-03-16 10:45:01 +0530

    Added remaining prgms

--- student1.java
+++ student1.java
@@ -1,4 +1,4 @@
-class student{
+class student1{
 
     //instance variable
     int rollNo;
@@ -8,26 +8,27 @@ class student{
     public static void main(String[] args) {
         //Syntax to create an object
         //Classname objreference = new classname()
-        student studentRef = new student();
+        student1 studentRef = new student1();
 
         //Initializing student values - values to students
         studentRef.name = "Ashwith Reddy";
         studentRef.rollNo = 141;
         studentRef.age = 21;
         studentRef.course = "Full stack"; 
-
-        //Printing all the values initialized
-        System.out.println(studentRef.name);
-        System.out.println(studentRef.rollNo);
-        System.out.println(studentRef.age);
-        System.out.println(studentRef.course);
+        studentRef.display();
+        student1 studentRef1 =new student1();
+        studentRef1.name="abd";
+        studentRef1.rollNo=148;
+        studentRef1.age=21;
+        studentRef1.course="python";
+        studentRef1.display();
+        
     }
     public void display(){
         System.out.println(name);
         System.out.println(age);
         System.out.println(rollNo);
         System.out.println(course);
+        System.out.println();
     }
-
-    
 }
